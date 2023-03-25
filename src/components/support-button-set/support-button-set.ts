1f30737ff28b4202c93c5e70e1e42a52bda59c78
Dale de Silva <daledesilva@gmail.com>
2023-03-25 16:02:49 +1100

    Tweaked support button set targets

--- src/components/support-button-set/support-button-set.ts
+++ src/components/support-button-set/support-button-set.ts
@@ -10,15 +10,15 @@ export class SupportButtonSet {
             btn.setTooltip('Message or follow developer');
             btn.setIcon('twitter');
             btn.onClick( (e) => {
-                window.open('https://twitter.com/daledesilva', 'twitter');
+                window.open('https://twitter.com/daledesilva', '_blank');
             })
         })
         SettingEl.addButton(btn => {
             btn.setClass('gki_button');
-            btn.setTooltip('Tip Developer');
+            btn.setTooltip('Support Developer');
             btn.setIcon('heart');
             btn.onClick( (e) => {
-                window.open('https://ko-fi.com/N4N3JLUCW', 'tip-tab');
+                window.open('https://ko-fi.com/N4N3JLUCW', '_blank');
             })
         })
     }
